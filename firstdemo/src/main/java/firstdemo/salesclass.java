7e5e02728aaf89e8979a10ab3846b4427b27e387
Hp <Hp@LAPTOP-0OON77O4>
2021-08-16 16:04:13 +0530

    commit all

--- firstdemo/src/main/java/firstdemo/salesclass.java
+++ firstdemo/src/main/java/firstdemo/salesclass.java
@@ -1,5 +1,14 @@
 package firstdemo;
 
 public class salesclass {
+	public void display() {
+		int[] arr= {2,3,5,6};
+		for(int num:arr)
+			System.out.println(num);
+	}
+	public void print() {
+		System.out.println("print all array numbers");
+		display();
+	}
 
 }
